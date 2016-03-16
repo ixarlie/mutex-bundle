bba203afe2b21421affecd1d3336f927ba7eb4a3
ixarlie <ixarlie@gmail.com>
2016-03-16 18:03:44 +0100

    initial commit

---
+++ IXarlieMutexBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace IXarlie\MutexBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class IXarlieMutexBundle
+ *
+ * @author Carlos Dominguez <ixarlie@gmail.com>
+ */
+class IXarlieMutexBundle extends Bundle
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        // @TODO extension configuration
+    }
+}
